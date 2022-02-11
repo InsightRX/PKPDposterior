28ace9d74194c88ee8cdb15deda0a5cb0fb778c4
Ron Keizer <ron@insight-rx.com>
2022-02-11 19:53:02 +0000

    update function + some postprocessing

--- R/get_mcmc_posterior.R
+++ R/get_mcmc_posterior.R
@@ -4,6 +4,9 @@
 #' @param mod compiled Stan model
 #' @param regimen drug regimen data created using `PKPDsim::new_regimen()`
 #' @param covariates patient covariate data created using `PKPDsim::covariates()`
+#' @param data data.frame with observed data.
+#' @param nm_data NONMEM-style dataset. If `nm_data` supplied, then `regimen`, `covariates`, and `data` are not required.
+#' @param seed seed for sampling
 #' @param output_dir 
 #' @param ... arguments passed to `mod$sample()`
 #' 
@@ -18,9 +21,12 @@
 #' @export
 get_mcmc_posterior <- function(
   mod,
-  regimen,
-  covariates,
-  data,
+  prior,
+  regimen = NULL,
+  covariates = NULL,
+  data = NULL,
+  nm_data = NULL, 
+  seed = 12345,
   output_dir = getwd(),
   ...
 ) {
@@ -30,23 +36,35 @@ get_mcmc_posterior <- function(
     stop("Supplied model is not a valid cmdstanr model. Please use `load_model()` to load/compile models.")
   }
   
-  ## Check data OK?
-  ## TODO
-  
   ## convert regimen, covariates, and data to NONMEM-like data
-  data <- prepare_data(
-    regimen,
-    covariates,
-    data
-  )
-  
+  if(!is.null(nm_data)) { # NONMEM-style data, can be passed directly to cmdstanr
+    stan_data <- nm_data  
+    ## TODO: check input data
+  } else {
+    if(is.null(regimen) || is.null(covariates) || is.null(data)) {
+      stop("Arguments `regimen`, `covariates`, and `data` are required. Alternatively, a NONMEM-style dataset can be provided using the `nm_data` argument")
+    }
+    stan_data <- prepare_data(
+      regimen,
+      covariates,
+      data
+    )
+  }
+
   res <- mod$sample(
-    data = data,
+    data = stan_data,
+    init = function() { prior },
+    seed = seed,
     output_dir = output_dir,
     ...
   )
   
-  ## Post-processing & return
-  res
-  
+  ## Post-processing
+  out <- list(
+    raw = res,
+    draws_df =  posterior::as_draws_df(res$draws())
+  )
+
+  ## return
+  out
 }
--- examples/ron_test.R
+++ examples/ron_test.R
@@ -1,5 +1,8 @@
 library(PKPDsim)
 library(pkpdmcmctbd)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
 
 # set Stan path
 Sys.setenv(STAN_PATH = "/home/ron@insight-rx.com/git/Torsten")
@@ -9,21 +12,54 @@ Sys.setenv(STAN_PATH = "/home/ron@insight-rx.com/git/Torsten")
 # at ~/R/x86_64-pc-linux-gnu-library/4.1/pkpdmcmctbd/models/
 mod <- load_model("pk2cmt", verbose = FALSE)
 
-## Patient data
-reg <- new_regimen()
-covs <- list(
-  WT = ..,
-  CRCL = ...
+## define prior. 
+## for our models this should be read from pkvancothomson::parameters() and 
+## pkvancothomson::ruv(). Should create a get_prior() fucntion.
+prior <- list( 
+  CL = 7.43,
+  ka = 1.08,
+  Q = 28.0,
+  sigma = 0.58,
+  V1 = 78.4,
+  V2 = 68.1
 )
-tdm_data <- data.frame()
+ 
+## Patient data using PKPDsim-style workflow:
+# reg <- new_regimen()
+# covs <- list(
+#   WT = ..,
+#   CRCL = ...
+# )
+
+## For now, using pre-created data from Torsten example
+source("examples/example_data.R")
 
 ## Sample from posterior
-post <- get_mcmc_posteriori(
+chains <- 2
+post <- get_mcmc_posterior(
   mod,
-  regimen = reg,
-  covariates = covs,
-  data = tdm_data
+  nm_data = nm_data,
+  prior = prior,
+  chains = chains,
+  parallel_chains = chains,
+  iter_warmup = 500,
+  iter_sampling = 500
+  # regimen = reg,
+  # covariates = covs,
+  # data = tdm_data
 )
 
-## Plotting
+## Plot parameters
+par_table <- post$draws_df %>%
+  select(CL, V1, Q, V2) %>%
+  pivot_longer(cols = c(CL, V1, Q, V2))
+prior_df <- data.frame(prior[c("CL", "V1", "Q", "V2")]) %>%
+  pivot_longer(cols = c(CL, V1, Q, V2))
+ggplot(par_table) +
+  geom_histogram(aes(x = value)) + 
+  facet_wrap(~name, scale = "free") +
+  geom_vline(data=prior_df, aes(xintercept = value), colour = 'red') +
+  irxreports::theme_irx_minimal()
+
+## Simulate using PKPDsim to get posterior for DV
 
